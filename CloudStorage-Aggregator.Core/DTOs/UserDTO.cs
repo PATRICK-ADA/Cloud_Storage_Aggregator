09f422008f27c0c756567d03c7c951b0f5e760c1
PATRICK-ADA <donpat646@gmail.com>
2023-11-23 00:43:25 +0100

    Recent_updates

---
+++ CloudStorage-Aggregator.Core/DTOs/UserDTO.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace DTOs.UserDTO
+{
+public class UserDTO
+{
+public string UserName{get; set;}
+public string Email {get; set:}
+
+public string Password{get; set;}
+
+
+}
+
+
+
+}
