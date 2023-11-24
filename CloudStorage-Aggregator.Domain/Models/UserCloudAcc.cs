912a3ea3b3a8d9ab49f1644c56f3285b7859bf89
PATRICK-ADA <donpat646@gmail.com>
2023-11-24 01:57:20 +0100

    Updates

--- CloudStorage-Aggregator.Domain/Models/UserCloudAcc.cs
+++ CloudStorage-Aggregator.Domain/Models/UserCloudAcc.cs
@@ -2,17 +2,17 @@ using System;
 using Google.Apis.Drive.v3;
 using System.Collections.Generic;
 
+
 namespace Models.UserCloudAcc
 {
 public class UserCloudAcc
 
 {
- public string ClientId { get; set; }
+    public string ClientId { get; set; }
     public string ClientSecret { get; set; }
     public string RedirectUri { get; set; }
     public List<DriveService> DriveServices { get; set; } = new List<DriveService>();
-
-public string UserId {get; set;}
+    
 
 }
 
